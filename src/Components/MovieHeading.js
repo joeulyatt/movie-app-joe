71d2ff4533cdfaa71a443dc1a7f5490066f58745
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-03-30 16:34:56 +0100

    fix bug to prevent movieList div from stretching horiztonally if less than 5 movies

--- src/Components/MovieHeading.js
+++ src/Components/MovieHeading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MovieHeading = ( {heading} ) => {
     return ( 
-        <div className="col">
+        <div className="col ms-3">
             <h1>{heading}</h1>
         </div>
     );
