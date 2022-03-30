18f4d452f521714566fd3521e09aa0975d23f055
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-03-30 16:25:47 +0100

    fix vertical scroll bug

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -4,7 +4,7 @@ const MovieList = ( {movies, FavouriteComponent, handleFavourites} ) => {
     return ( 
         <>
             {movies.map((movie, idx) => (
-                <div className="image-container d-flex justify-content-start m-3" key={idx}>
+                <div className="image-container d-flex justify-content-start m-2 " key={idx}>
                     <img src={movie.Poster} alt={movie.Title} srcSet=""></img>
                     <div onClick={() => handleFavourites(movie)} className="overlay d-flex align-items=center justify-content-center">
                         <FavouriteComponent/>
