7961216f375d99c82c719a48f19d7a9be9064b5c
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-03-30 11:21:30 +0100

    map over movie array in MovieList.js and mount

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const MovieList = ( {movies} ) => {
     return ( 
-        
+        <>
+            {movies.map((movie, idx) => (
+                <div>
+                    <img src={movie.Poster} alt={movie.imdbID} srcset=""></img>
+                </div>
+            ))}
+        </>
     );
 }
- 
+
 export default MovieList;
