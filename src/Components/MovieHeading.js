f2e9af32eca2dccee5a8dcb85d7a84d9def5767a
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-03-30 12:09:02 +0100

    create MovieHeading component

---
+++ src/Components/MovieHeading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MovieHeading = () => {
+    return ( 
+        <div className="col">
+            <h1></h1>
+        </div>
+    );
+}
+ 
+export default MovieHeading;
