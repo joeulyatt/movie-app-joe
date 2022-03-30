b14eea851135f9e930d7cb65778fad4eb5e22ace
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-03-30 11:14:17 +0100

    add state to app with inital state from omdb API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hey
-    </div>
-  );
+const App = () => {
+  const [movies, setMovies] = useState([])
+  return <div>Hello World</div>
 }
 
 export default App;
