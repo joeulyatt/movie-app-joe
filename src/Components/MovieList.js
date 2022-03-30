793e952d475533f2743142ad34da5a978b2670cf
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-03-30 14:25:44 +0100

    link handleAddFavourite to MovieList onclick

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const MovieList = ( {movies, FavouriteComponent} ) => {
+const MovieList = ( {movies, FavouriteComponent, handleAddFavourite} ) => {
     return ( 
         <>
             {movies.map((movie, idx) => (
                 <div className="image-container d-flex justify-content-start m-3" key={movie.imdbID}>
                     <img src={movie.Poster} alt={movie.Title} srcSet=""></img>
-                    <div className="overlay d-flex align-items=center justify-content-center">
+                    <div onClick={() => handleAddFavourite(movie)} className="overlay d-flex align-items=center justify-content-center">
                         <FavouriteComponent/>
                     </div>
                 </div>
