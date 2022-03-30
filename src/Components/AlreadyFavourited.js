e6de7cdfd401aa30c7c1f611962ec0649daf54d9
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-03-30 16:07:32 +0100

    add alreadyFavourited component

---
+++ src/Components/AlreadyFavourited.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddFavourites = () => {
+    return ( 
+        <>
+            <span className="mr-2">Already favourited!
+            </span>
+
+        </>
+    );
+};
+
+export default AddFavourites;
