3caf3a4576c6d4903e9011263dfb64aa5e920cdd
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-03-30 13:13:29 +0100

    add hover effect to movies, makes movies bigger

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -4,8 +4,9 @@ const MovieList = ( {movies} ) => {
     return ( 
         <>
             {movies.map((movie, idx) => (
-                <div className="d-flex justify-content-start m-3" key={movie.imdbID}>
+                <div className="image-container d-flex justify-content-start m-3" key={movie.imdbID}>
                     <img src={movie.Poster} alt={movie.Title} srcSet=""></img>
+                    <div className="overlay d-flex align-items=center justify-content-center"></div>
                 </div>
             ))}
         </>
