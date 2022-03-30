b3ab70aba2217ae2eb7611bd676eb33c01cd8fae
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-03-30 11:56:42 +0100

    move key to correct place

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -4,8 +4,8 @@ const MovieList = ( {movies} ) => {
     return ( 
         <>
             {movies.map((movie, idx) => (
-                <div className="d-flex justify-content-start m-3">
-                    <img src={movie.Poster} alt={movie.Title} key={movie.imdbID} srcset=""></img>
+                <div className="d-flex justify-content-start m-3" key={movie.imdbID}>
+                    <img src={movie.Poster} alt={movie.Title} srcSet=""></img>
                 </div>
             ))}
         </>
