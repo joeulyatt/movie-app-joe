dafae3fd0ba4bfc6ac4a56e5f75160186adf6a92
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-03-31 18:12:32 +0100

    use idx so plot only appears on clicked movie

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -3,14 +3,16 @@ import React, { useState } from 'react';
 const MovieList = ( {movies, FavouriteComponent, handleFavourites} ) => {
     const [plot, setPlot] = useState("");
     const [showPlot, setShowPlot] = useState(false);
+    const [plotIdx, setPlotIdx] = useState(); 
 
     const getMovieInfo = async (movie, idx) => {
         const url = `http://www.omdbapi.com/?t=${movie.Title}&apikey=ca4d40cb`;
         const response = await fetch(url);
         const json = await response.json();
-        setPlot(json.Plot)
-        setShowPlot(true)
-        console.log(idx)
+        setPlot(json.Plot);
+        setShowPlot(true);
+        setPlotIdx(idx);
+        console.log(plotIdx)
     };
     
     return ( 
@@ -18,7 +20,7 @@ const MovieList = ( {movies, FavouriteComponent, handleFavourites} ) => {
             {movies.map((movie, idx) => (
                 <div className="image-container d-flex justify-content-start m-3 " key={idx}>
                             <div className="popup">
-                                <span className={showPlot ? "show popuptext" : null} id="myPopup">{plot}</span>
+                                <span className={showPlot ? "show popuptext" : null} id="myPopup">{idx === plotIdx ? plot : null}</span>
                             </div>
                     <img src={movie.Poster} alt={movie.Title} srcSet=""></img>
                     {/* Checks init pic and displays AddToFavourites if false */}
