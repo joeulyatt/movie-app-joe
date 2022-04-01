0e8d3ce239e180a1cee6f5c0bdb6ba2420bcc0ce
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-01 16:12:20 +0100

    refactor Searchbox.js

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
-const SearchBox = ( {val, setVal}) => {
-    return ( 
+const SearchBox = ( {val, setVal}) => 
         <div className="col col-sm-4">
             <input 
-                className="form-control" 
                 type="text" 
+                className="form-control" 
                 placeholder="Search Here"
                 value={val}
                 onChange={e => setVal(e.target.value)}
             ></input>
         </div>
-    );
-};
 
 export default SearchBox;
