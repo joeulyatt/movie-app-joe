9786c20d213f441abedf5cbcfa54a434416279dd
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-01 16:06:50 +0100

    refactor AlreadyFavourited.js

--- src/Components/AlreadyFavourited.js
+++ src/Components/AlreadyFavourited.js
@@ -1,12 +1,5 @@
 import React from 'react'
 
-const AlreadyFavouritedComponent = () => {
-    return ( 
-        <>
-            <span className="mr-2 fs-6">Already favourited!
-            </span>
-        </>
-    );
-};
+const AlreadyFavouritedComponent = () => <span className="mr-2 fs-6">Already favourited!</span>
 
 export default AlreadyFavouritedComponent;
