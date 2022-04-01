b8e5699c4c8b69368b7ade7c2751fc1d421855de
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-01 11:24:32 +0100

    add info and favourite styling

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 
 const MovieList = ( {movies, FavouriteComponent, handleFavourites} ) => {
     const [plot, setPlot] = useState("");
+    const [info, setInfo] = useState("");
     const [showPlot, setShowPlot] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
     const [plotIdx, setPlotIdx] = useState(); 
 
     const getMovieInfo = async (movie, idx) => {
         const url = `http://www.omdbapi.com/?t=${movie.Title}&apikey=ca4d40cb`;
         const response = await fetch(url);
         const json = await response.json();
+        console.log(json)
         setPlot(json.Plot);
+        setInfo(json);
         setShowPlot(true);
+        setShowInfo(true);
         setPlotIdx(idx);
     };
     
@@ -18,25 +23,51 @@ const MovieList = ( {movies, FavouriteComponent, handleFavourites} ) => {
         <>
             {movies.map((movie, idx) => (
                 <div className="image-container d-flex justify-content-start m-3 " key={idx}>
-                    {/* Shows movie plot when clicked, hides when clicked */}
                     <div className="popup">
-                        <span 
-                            className={showPlot && idx === plotIdx ? "show popuptext" : null} 
-                            id="myPopup"
-                            onClick={() => setShowPlot(false)}
-                        >
-                            {idx === plotIdx && showPlot ? plot : null}
-                        </span>
+                        <div>
+                            {/* Shows Info when clicked, hides when clicked */}
+                            <span
+                                className={showInfo && idx === plotIdx ? "text-center myInfo show popuptext" : null}
+                                id="myPopup"
+                                onClick={() => setShowInfo(false)}
+                            >
+                                
+                                {idx === plotIdx && showInfo ?
+                                    (
+                                    <>
+                                        <div>Year: {info.Year}</div>
+                                        <div>RunTime: {info.Runtime}</div>
+                                        <div>Genre: {info.Genre}</div>
+                                        <div>Actors: {info.Actors}</div>
+                                    </>
+                                    )
+                                : null}
+                            </span>
+                            {/* Shows movie plot when clicked, hides when clicked */}
+                            <span
+                                className={showInfo && idx === plotIdx ? "myPlot show popuptext" : null}
+                                id="myPopup"
+                                onClick={() => setShowPlot(false)}
+                            >
+                                {idx === plotIdx && showInfo ? plot : null}
+                            </span>
+                        </div>
+
                     </div>
                     <img src={movie.Poster} alt={movie.Title} srcSet=""></img>
                     {/* Checks init pic and displays AddToFavourites if false */}
                     {FavouriteComponent !== null ?
                         <>
-                            <div onClick={() => getMovieInfo(movie, idx)} className="overlay d-flex align-items=center justify-content-center">
-                                <h2>Plot</h2>
-                            </div>
-                            <div onClick={() => handleFavourites(movie)} className="overlay putToBottom d-flex align-items=center justify-content-center">
-                                <FavouriteComponent/>
+                            <div class="d-flex overlay w-100">
+                                <div onClick={() => getMovieInfo(movie, idx)} className="col">
+                                    <h2>Info</h2>
+                                </div>
+                                {/* <div onClick={() => getMovieInfo(movie, idx)} className="info-overlay overlay d-flex align-items=center justify-content-center">
+                                    <h2>Info</h2>
+                                </div> */}
+                                <div onClick={() => handleFavourites(movie)} className="col">
+                                    <FavouriteComponent/>
+                                </div>
                             </div>
                         </>
                     : null}
