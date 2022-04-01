6fce6a348b52e042fc974f9303bd36ee43348f1f
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-01 16:07:27 +0100

    refactor Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,5 @@
 import React from 'react'
 
-const Footer = () => {
-    return ( 
-        <footer>
-            <div className= "text-center mt-4 mb-4">Copyright 	&copy; 2022 JoeWhoCodes</div>
-        </footer>
-     );
-}
- 
+const Footer = () => <footer><div className= "text-center mt-4 mb-4">Copyright &copy; 2022 JoeWhoCodes</div></footer>
+
 export default Footer;
