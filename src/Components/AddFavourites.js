4767e96073c8d68192c86229c6c0d0bb4463b9c8
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-04 09:59:40 +0100

    refactor RemoveFavourite, AddFavourites with Bootstrap icons

--- src/Components/AddFavourites.js
+++ src/Components/AddFavourites.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const AddFavourites = () => <i id="myicon" class="bi bi-suit-heart-fill"></i>
+const AddFavourites = () => <i class="myIcon bi bi-suit-heart-fill"></i>
 
 export default AddFavourites;
