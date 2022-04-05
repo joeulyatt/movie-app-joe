425f4c18d8c166a2ea7cc1c6615ab399770dd378
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-05 10:09:45 +0100

    create tab component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import TabGroup from './Components/TabGroup'
 import MovieList from './Components/MovieList';
 import MovieHeading from './Components/MovieHeading';
 import SearchBox from './Components/SearchBox';
@@ -14,11 +15,21 @@ import Logo from './img/logo.png';
 
 const App = () => {
     const [movies, setMovies] = useState([]);
+    const [shows, setShows] = useState([]);
     const [trending, setTrending] = useState([])
     const [favourites, setFavourites] = useState([{poster_path: DefaultFavourite}]);
     const [val, setVal] = useState("");
     const [initPic, setInitPic] = useState(true);
     const [initFavouritePic, setInitFavouritePic] = useState(true);
+
+    const getShowRequest = async (val) => {
+        const url=`https://api.themoviedb.org/3/search/tv?api_key=50eda2eddd31465d5fbf9f1c49d7b8a6&language=en-US&page=1&include_adult=false&query=${val}`
+        const response = await fetch(url);
+        const json = await response.json();
+        setShows(json.results ? json.results : [{poster_path: DefaultPic}]);
+        // setInitPic(json.results ? false : true);
+    };
+
 
     const getMovieRequest = async (val) => {
         const url=`https://api.themoviedb.org/3/search/movie?api_key=50eda2eddd31465d5fbf9f1c49d7b8a6&query=${val}`
@@ -41,6 +52,7 @@ const App = () => {
 
     useEffect(() => {
         getMovieRequest(val);
+        getShowRequest(val);
     }, [val]);
 
     const handleAddFavourite = (movie, idx) => {
@@ -62,10 +74,23 @@ const App = () => {
 
     return (
         <div className="container-fluid movie-app">
+            <TabGroup/>
             <div className="col d-flex justify-content-center">
                 <img src={Logo} alt="" srcSet=""></img>
             </div>
             <hr></hr>
+            <div className="row d-flex align-items-center ps-3 mt-4 mb-4 me-4">
+                <MovieHeading heading="TV Shows"/>
+                <SearchBox val={val} setVal={setVal}/>
+            </div>
+            <div className="row movies flex-nowrap">
+                <MovieList 
+                    movies={shows} 
+                    handleFavourites={handleAddFavourite} 
+                    FavouriteComponent={AddFavourites}
+                    favourites={favourites}
+                />
+            </div>
             <div className="row d-flex align-items-center ps-3 mt-4 mb-4 me-4">
                 <MovieHeading heading="Trending"/>
             </div>
