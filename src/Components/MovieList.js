d019150283fa93102bb6a96544f1f0297fac9137
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-06 13:02:16 +0100

    remove info section

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,19 +1,9 @@
 import React, { useEffect, useState } from 'react';
 
 const MovieList = ( {movies, FavouriteComponent, handleFavourites, favourites, alreadyFavourited} ) => {
-    const [info, setInfo] = useState("");
     const [video, setVideo] = useState("");
     const [showInfo, setShowInfo] = useState(false);
     const [movieIdx, setMovieIdx] = useState(); 
-
-    const getMovieInfo = async (movie, idx) => {
-        const url = `https://api.themoviedb.org/3/movie/${movie.id}?api_key=50eda2eddd31465d5fbf9f1c49d7b8a6&language=en-US`
-        const response = await fetch(url);
-        const json = await response.json();
-        setInfo(json);
-        setMovieIdx(idx);
-        setShowInfo(!showInfo);
-    };
 
     const getMovieVideo = async (movie, idx) => {
         const url = `https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=50eda2eddd31465d5fbf9f1c49d7b8a6&language=en-US`
@@ -42,29 +32,17 @@ const MovieList = ( {movies, FavouriteComponent, handleFavourites, favourites, a
             {movies.map((movie, idx) => (
                 <div className="image-container d-flex justify-content-start m-3"  onMouseEnter={() => getMovieVideo(movie, idx)}  key={idx}>
                     {idx === movieIdx && showInfo ?
-                        <>
-                        {/* <div className="popup text-center" onClick={() => setShowInfo(false)}>
-                            <span className="myPlot show popuptext">{info.overview}</span>
-                        </div> */}
-                        <iframe src={`https://www.youtube.com/embed/${video}`}></iframe>
-                        </>
+                        <iframe src={`https://www.youtube.com/embed/${video}?controls=0`} className="overlay myVideo"></iframe>
                     : null}
                     <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.Title} srcSet=""></img>
                     {FavouriteComponent !== null ?
-                        <>
-                            <div className="d-flex overlay w-100">
-                                <div  className="col">
-                                    <h2 className="myInfo">Info</h2>
-                                </div>
-                                <div onClick={() => handleFavourites(movie, idx)} className="col">
+                                <div onClick={() => handleFavourites(movie, idx)} className="overlay">
                                     <FavouriteComponent 
                                         alreadyFavourited={alreadyFavourited}
                                         favourites={favourites}
                                         movie={movie}
                                     />
                                 </div>
-                            </div>
-                        </>
                     : null}
                 </div>
             ))}
