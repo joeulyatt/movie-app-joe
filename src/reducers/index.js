d389ee239ed083243c43ffccf67b408d8ec28128
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-12 13:19:10 +0100

    final commit before starting React Router branch

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import favouriteReducer from "./favourites";
-import { combineReducers } from "redux";
-
-const allReducers = combineReducers({
-    watchlist : favouriteReducer
-});
-
-export default allReducers;
