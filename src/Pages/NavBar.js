75fdb9d05ac92d9ee0f5a82b02965bdf5e54dd03
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-12 14:43:24 +0100

    update Navbar styling and formatting

--- src/Pages/NavBar.js
+++ src/Pages/NavBar.js
@@ -8,14 +8,21 @@ const NavBar = () => {
     const [val, setVal] = useState("");
 
     return ( 
-        <nav className="container-fluid movie-app">
-            <div className="d-flex justify-content-between">
-                <div className="mt-2 mb-2">
-                    <img src={Logo} alt="" srcSet="" height="100px" className="me-5"/>     
-                </div>
-                <SearchBox/>
-            </div>    
-        </nav>
+        <nav>
+            <img src={Logo} alt="" srcSet="" height="100px"/>     
+            <ul className="nav-links">
+                <Link to="/Movies">
+                    <li>Movies</li>
+                </Link>
+                <Link to="/TVShows">
+                    <li>TV Shows</li>
+                </Link>
+                <Link to="/Watchlist">
+                    <li>Watchlist</li>
+                </Link>
+            </ul>
+            <SearchBox/>
+        </nav> 
     );
 };
 
