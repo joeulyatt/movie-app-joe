a6fe592f2fe50fa742b424a52eda66efe5678a93
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-14 16:08:27 +0100

    map over MovieTypes to create all movieHeaders

--- src/Pages/Movies.js
+++ src/Pages/Movies.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
 import { useSelector, useDispatch } from 'react-redux';
 import { getMoviesAsync } from '../Redux/moviesSlice';
+import MovieHeading from '../Components/MovieHeading';
+
+const movieTypes = ['Trending Today', 'Comedy', 'Horror', 'Action', 'Drama'];
 
 const Movies = () => {
     const movies = useSelector(state => state.movies);
@@ -12,12 +16,34 @@ const Movies = () => {
     }, [dispatch]);
 
     return ( 
-        <div>
-            <h1>Movies</h1>
-            {movies.map((e) => (
-                <img src ={`https://image.tmdb.org/t/p/w200/${e.poster_path}`}></img>
-            ))}    
-        </div>
+        <>
+        {movieTypes.map((types) => (
+            <div>
+                <MovieHeading heading={types}/>
+                {movies.map((e) => (
+                    <img src ={`https://image.tmdb.org/t/p/w200/${e.poster_path}`}></img>
+                ))}    
+            </div>
+        ))}
+
+            {/* <MovieHeading heading={}/> */}
+            <div className="row movies flex-nowrap">
+                {/* <MovieList 
+                        movies={handleType(types)} 
+                        favourites={favourites}
+                        handleAddFavourite={handleAddFavourite} 
+                        handleRemoveFavourite={handleRemoveFavourite} 
+                        FavouriteComponent={AddFavourites}
+                        activeTab={activeTab}
+                    /> */}
+            </div>
+            <div>
+                <h1>Movies</h1>
+                {movies.map((e) => (
+                    <img src ={`https://image.tmdb.org/t/p/w200/${e.poster_path}`}></img>
+                ))}    
+            </div>
+        </>
     );
 };
 
