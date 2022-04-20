e684eac922cf8a65d6559249ff483c94c794abf3
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-20 10:41:25 +0100

    tidy MoviesPage.js and move movieTypes object to separate data file

--- src/Pages/MoviesPage.js
+++ src/Pages/MoviesPage.js
@@ -3,74 +3,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
 import { useSelector, useDispatch } from 'react-redux';
 import { getMoviesAsync } from '../Redux/moviesSlice';
-import MovieList from '../Components/MovieList';
-
-const movieTypes = ['Trending Today', 'Comedy', 'Horror', 'Action', 'Drama'];
-const genres = [35, 27];
-
-const movieTypes2 = [
-    {
-        genre: "Trending Today",
-        type: "trending",
-        format: "movie/day",
-        code: "",
-    },
-    {
-        genre: "Comedy",
-        type: "discover",
-        format: "movie",
-        code: 35,
-    },
-    {
-        genre: "Horror",
-        type: "discover",
-        format: "movie",
-        code: 27,
-    },
-    {
-        genre: "Action",
-        type: "discover",
-        format: "movie",
-        code: 28,
-    },
-    {
-        genre: "Drama",
-        type: "discover",
-        format: "movie",
-        code: 18,
-    }
-]
-
+import { movieTypes } from '../data/movieTypes';
 
 const MoviesPage = () => {
     const movies = useSelector(state => state.movies);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        movieTypes2.forEach(e => {       
+        movieTypes.forEach(e => {       
             dispatch(getMoviesAsync(e));     
         });
     }, [dispatch]);
 
-
-
     return ( 
         <>
-        {movieTypes.map((types, index) => (
-            <div className="movies-list">
-                <h2>{types}</h2>
-                <div className="row movies flex-nowrap">
-                {movies.length === 0 ? null : 
-                    movies[index].map((m) => (
-                        <div className="">
-                            <img src ={`https://image.tmdb.org/t/p/w200/${m.poster_path}`}></img>
-                        </div>
-                    ))   
-                }
-
+            {movieTypes.map((types, index) => (
+                <div className="movies-list">
+                    <h2>{types.genre}</h2>
+                    <div className="row movies flex-nowrap">
+                        {movies.length === 0 ? null : 
+                            movies[index].map((m) => (
+                                <div className="">
+                                    <img src ={`https://image.tmdb.org/t/p/w200/${m.poster_path}`} alt={m.title}></img>
+                                </div>
+                            )) 
+                        }
+                    </div>
                 </div>
-            </div>
-        ))}
+            ))}
         </>
     );
 };
---
+++ src/data/movieTypes.js
@@ -0,0 +1,32 @@
+export const movieTypes = [
+    {
+        genre: "Trending Today",
+        type: "trending",
+        format: "movie/day",
+        code: "",
+    },
+    {
+        genre: "Comedy",
+        type: "discover",
+        format: "movie",
+        code: 35,
+    },
+    {
+        genre: "Horror",
+        type: "discover",
+        format: "movie",
+        code: 27,
+    },
+    {
+        genre: "Action",
+        type: "discover",
+        format: "movie",
+        code: 28,
+    },
+    {
+        genre: "Drama",
+        type: "discover",
+        format: "movie",
+        code: 18,
+    }
+];
