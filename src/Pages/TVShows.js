f8e779fc23ffb921fc1a5501b5307a1d1e9d4613
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-20 10:51:31 +0100

    copy MoviesPage logic over to TVPage logic

--- src/Pages/TVShows.js
+++ src/Pages/TVShows.js
@@ -1,26 +1,38 @@
 import React, { useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
 import { useSelector, useDispatch } from 'react-redux';
 import { getMoviesAsync } from '../Redux/moviesSlice';
+import { tvTypes } from '../data/tvTypes';
 
-
-const TVShows = () => {
+const TVShowsPage = () => {
     const movies = useSelector(state => state.movies);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(getMoviesAsync(28));
+        tvTypes.forEach(e => {       
+            dispatch(getMoviesAsync(e));     
+        });
     }, [dispatch]);
 
-
     return ( 
-        <div>
-            <h1>TV shows</h1>
-            {movies.map((e) => (
-            <img src ={`https://image.tmdb.org/t/p/w200/${e.poster_path}`}></img>
+        <>
+            {tvTypes.map((types, index) => (
+                <div className="movies-list">
+                    <h2>{types.genre}</h2>
+                    <div className="row movies flex-nowrap">
+                        {movies.length === 0 ? null : 
+                            movies[index].map((m) => (
+                                <div className="">
+                                    <img src ={`https://image.tmdb.org/t/p/w200/${m.poster_path}`} alt={m.title}></img>
+                                </div>
+                            )) 
+                        }
+                    </div>
+                </div>
             ))}
-        </div>
+        </>
     );
 };
 
-export default TVShows;
+export default TVShowsPage;
