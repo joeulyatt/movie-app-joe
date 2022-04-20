6b14896ec2dabb3dd3c6187a9562a47fe7d7c28e
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-20 14:51:52 +0100

    change template string so video url always matches media_type

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,20 +6,21 @@ const Card = ( {results, index, page, FavouriteComponent, activeTab} ) => {
     const newResults = (page === "watchlist" ? results : results[index])
 
     const getTrailer = async (movie, idx) => {
-        const url = `https://api.themoviedb.org/3/${page}/${movie.id}/videos?api_key=50eda2eddd31465d5fbf9f1c49d7b8a6&language=en-US`
+        const url = `https://api.themoviedb.org/3/${movie.media_type}/${movie.id}/videos?api_key=50eda2eddd31465d5fbf9f1c49d7b8a6&language=en-US`
         const response = await fetch(url);
         const json = await response.json();
         const key = json.results.find(e => e.name.includes("Trailer")).key
         setVideo(key)
         setCardIdx(idx);
+        console.log(movie)
     };
 
     return ( 
         <>
             {newResults.map((movie, idx) => (
                 <div className="movies image-container d-flex justify-content-start m-3" onMouseEnter={() => getTrailer(movie, idx)} key={movie.id}>
-                {idx === cardIdx ?
-                        <iframe src={`https://www.youtube.com/embed/${video}?controls=0`} title={movie.title} className="overlay myVideo"></iframe>
+                    {idx === cardIdx ?
+                        <iframe src={`https://www.youtube.com/embed/${video}?controls=0`} title={movie.title} className="overlay myVideo"/>
                     : null}
                     <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.Title} srcSet=""></img>
                     <div className="overlay">
--- src/Pages/Watchlist.js
+++ src/Pages/Watchlist.js
@@ -6,18 +6,13 @@ import { useSelector } from 'react-redux';
 const Watchlist = () => {
     const watchlist = useSelector(state => state.watchlist);
 
-    useEffect(() => {
-        console.log(watchlist[1])
-    } )
     return ( 
-        
         <div className="movies-list">
             <div className="row movies flex-nowrap">
-                    <Card
-                        results={watchlist}
-                        index=""
-                        page="watchlist"
-                    />
+                <Card
+                    results={watchlist}
+                    page="watchlist"
+                />
             </div>
         </div>
     );
