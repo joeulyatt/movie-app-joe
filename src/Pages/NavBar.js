ac2d2c63ed0f2e1708017d28486fce153a524e02
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-21 12:50:39 +0100

    refactor navLinks to use React Router Navlink

--- src/Pages/NavBar.js
+++ src/Pages/NavBar.js
@@ -1,37 +1,24 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../App.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import SearchBox from '../Components/SearchBox'
 import Logo from '../img/logo.png';
 
 const NavBar = () => {
-    const [active, setActive] = useState("Movies");
 
     return ( 
         <nav>
             <img src={Logo} alt="Logo" srcSet="" height="100px"/>     
             <ul className="nav-links">
-                <Link to="/Movies">
-                    <li 
-                        onClick={() =>setActive("Movies")}
-                        className={active === "Movies" ? "myActiveLink" : null}
-                    >   Movies
-                    </li>
-                </Link>
-                <Link to="/TVShows">
-                    <li 
-                        onClick={() =>setActive("TV")}
-                        className={active === "TV" ? "myActiveLink" : null}
-                    >   TV Shows
-                    </li>
-                </Link>
-                <Link to="/Watchlist">
-                <li 
-                        onClick={() =>setActive("Watchlist")}
-                        className={active === "Watchlist" ? "myActiveLink" : null}
-                    >   Watchlist
-                    </li>
-                </Link>
+                <NavLink to="/Movies">
+                    <li>Movies</li>
+                </NavLink>
+                <NavLink to="/TVShows">
+                    <li>TV Shows</li>
+                </NavLink>
+                <NavLink to="/Watchlist">
+                    <li>Watchlist</li>
+                </NavLink>
             </ul>
             <SearchBox/>
         </nav> 
