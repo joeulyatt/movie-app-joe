ebc2eabaa82ab3852ea8ae7f44850385e78d71c4
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-21 12:37:42 +0100

    add searchVal logic to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './Pages/NavBar';
@@ -10,25 +11,16 @@ import Search from './Pages/Search';
 import Footer from './Components/Footer';
 
 const App = () => {
-    const [search, setSearch] = useState([]);
-    const [val, setVal] = useState("");
-
-    const getJson = async (genre, type, val) => {
-        const url = `https://api.themoviedb.org/3/${type}/movie?api_key=50eda2eddd31465d5fbf9f1c49d7b8a6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres=${genre}&query=${val}`
-        const response = await fetch(url);
-        const json = await response.json();
-        return json;
-    };
+    const searchVal = useSelector(state => state.search);
 
     useEffect(() => {
-        if (val) {
-            (async () => {
-                const e = await getJson(null, "search", val)
-                setSearch(e.results ? e.results.filter(e => e.poster_path !== null) : []);
-            })()
+        if (searchVal.length > 0) {
+            console.log("searching")
+        } else {
+            console.log("not seraching")
         }
-        else {setSearch([])}
-    }, [val]);
+    }, [searchVal])
+
 
     return (
         <div className="container-fluid movie-app">
