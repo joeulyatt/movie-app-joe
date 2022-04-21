250c871bae8dff627c24c9a4459c4416a9821144
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-21 16:55:12 +0100

    add logic to go back a page in browser, but is currently unpredictable

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { getSearchAsync, reset } from '../Redux/searchSlice';
 
 const SearchBox = () => {
-    const dispatch = useDispatch();
     const [searchParams, setSearchParams] = useSearchParams();
+    const [isSearching, setIsSearching] = useState(false)
+    const dispatch = useDispatch();
     const navigate = useNavigate();
+    
+    useEffect(() => {
+        isSearching ? navigate("../Search", { replace: true }) : navigate(-1);
+    }, [isSearching])
 
     const getSearch = (e) => {
         dispatch(reset());
         dispatch(getSearchAsync(e));
-        navigate("../Search", { replace: true })
+        e ? setIsSearching(true) : setIsSearching(false);
+        // if (e) {navigate("../Search", { replace: true })} else navigate(-1);
     };
 
     return (
