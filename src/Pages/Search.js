85daad5397ba184809a15dfeec1ea673ba06c376
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-04-22 16:48:02 +0100

    add movie/tv filter to search page

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
 import Card from '../Components/Cards';
+import { useLocation } from 'react-router-dom';
 
 const SearchPage = () => {
     const searchResults = useSelector(state => state.search);
+    const [filteredResults, setFilteredResults] = useState([]);
+    const location = useLocation();
+
+    const filterMovies = () => {
+        setFilteredResults(searchResults.filter(e => e.media_type === "movie"));
+    };
+
+    const filterTV = () => {
+        setFilteredResults(searchResults.filter(e => e.media_type === "tv"));  
+    };
+
+    const filterAll = () => {
+        setFilteredResults(searchResults);
+    };
+
+    useEffect(() => {
+        setFilteredResults([]);
+    }, [location])
+
 
     return ( 
         <div className="movies-list">
+        <h2 className="text-end me-5">Filter
+            <button onClick={() => filterMovies()}>Movies</button>
+            <button onClick={() => filterTV()}>TV</button>
+            <button onClick={() => filterAll()}>All</button>
+        </h2>
             <div className="row movies">
                 <Card
-                    results={searchResults}
+                    results={filteredResults.length === 0 ? searchResults : filteredResults}
                     page="watchlist"
                 />
             </div>
