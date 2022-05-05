ba4335a93a7cb4daa0d5a245e6d94cf17b0ce489
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-05-05 13:10:29 +0100

    import auth and message slices to store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,11 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import moviesSlice from './moviesTvSlice';
 import watchlistSlice from './watchlistSlice';
 import searchSlice from './searchSlice';
+import authSlice from './authSlice';
+import messageSlice from './messageSlice';
 
 export default configureStore({
     reducer: {
         moviesTv: moviesSlice,
         watchlist: watchlistSlice,
-        search: searchSlice
+        search: searchSlice,
+        auth: authSlice,
+        message: messageSlice,
     }
 });
